0073d3b12721f418241b3726dce926361396331e
Guido M <guidojmaldonado@gmail.com>
2019-11-27 13:35:26 -0500

    (Guido) Incompleted work

--- app/src/main/java/com/example/volunteeringapp/MockData.kt
+++
@@ -1,9 +0,0 @@
-package com.example.volunteeringapp
-
-class MockData {
-    companion object {
-        fun getEvents() {
-
-        }
-    }
-}
--- app/src/main/java/com/example/volunteeringapp/MyEventsActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.example.volunteeringapp
-
-import androidx.appcompat.app.AppCompatActivity
-
-class MyEventsActivity : AppCompatActivity() {
-
-}
